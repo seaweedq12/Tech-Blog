daf9be2806742897de5e2563be0787ea969073c6
Unknown <84217207+seaweedq12@users.noreply.github.com>
2021-08-31 18:06:55 +1000

    intial commit with handlebar

---
+++ seeds/seed.js
@@ -0,0 +1,21 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const blog = await Blog.bulkCreate(blogData);
+  
+
+  process.exit(0);
+};
+
+seedDatabase();
